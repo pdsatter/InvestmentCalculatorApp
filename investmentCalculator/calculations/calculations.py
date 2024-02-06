f8ad2c4f65e8d8b5ed6cea04273eca425e25dd1e
pdsatter <87093940+pdsatter@users.noreply.github.com>
2024-02-06 11:24:06 -0600

    Reupload files in folder

---
+++ investmentCalculator/calculations/calculations.py
@@ -0,0 +1,38 @@
+class YearlyInfo():
+    def __init__(self, startingContributedFunds, startingInterestFunds, year=1):
+        self.startingContributedFunds = startingContributedFunds
+        self.startingInterestFunds = startingInterestFunds
+        self.startingTotalFunds = startingContributedFunds + startingInterestFunds
+
+        self.finalContributedFunds = None
+        self.finalInterestFunds = None
+        self.finalTotalFunds = None
+        self.year = year
+
+    def calculateYear(self, annualContribution, returnRate):
+        self.finalContributedFunds = self.startingContributedFunds + annualContribution
+
+        yearStartFunds = self.finalContributedFunds + self.startingInterestFunds
+        self.finalInterestFunds = self.startingInterestFunds + returnRate * yearStartFunds
+
+        self.finalTotalFunds = self.finalInterestFunds + self.finalContributedFunds
+
+def calculate(initialFunds, annualContribution, yearsContributing, returnRate):
+    firstYear = YearlyInfo(initialFunds, 0)
+    firstYear.calculateYear(annualContribution, returnRate)
+
+    yearly_data = [firstYear]
+
+    return recursive_calculate_yearly_data(yearly_data, annualContribution, returnRate, yearsContributing-1)
+
+def recursive_calculate_yearly_data(yearly_data, annualContribution, returnRate, yearsLeft):
+    if (yearsLeft <= 0): return yearly_data
+
+    pastYear = yearly_data[-1]
+
+    currentYear = YearlyInfo(pastYear.finalContributedFunds, pastYear.finalInterestFunds, pastYear.year+1)
+    currentYear.calculateYear(annualContribution, returnRate)
+
+    yearly_data.append(currentYear)
+    
+    return recursive_calculate_yearly_data(yearly_data, annualContribution, returnRate, yearsLeft-1)
---
+++ investmentCalculator/tk_canvas.py
@@ -0,0 +1,50 @@
+class CanvasDrawer():
+    def __init__(self, canvas, width, height):
+        self.canvas = canvas
+
+        self.height = height
+        self.width = width
+    
+    def clear(self):
+        self.canvas.delete("all")
+
+    def draw_years(self, years_data):
+        self.clear()
+
+        for year in years_data:
+            self.draw_year(year, years_data)    
+
+    def draw_year(self, year_data, years_data):
+        height1 = year_data.finalContributedFunds
+        height2 = year_data.finalInterestFunds
+
+        y_max = self.calc_max_height(years_data)
+
+        y1 = self.height - (self.calc_normalized_height(y_max, height1))
+        y2 = self.height - (self.calc_normalized_height(y_max, height2) + self.calc_normalized_height(y_max, height1))
+
+        width = self.calc_normalized_width(len(years_data))
+
+        x1 = width * (year_data.year - 1)
+        x2 = width * (year_data.year)
+
+        self.draw_rectangle(x1,self.height,x2,y1)
+        self.draw_rectangle(x1,y1,x2,y2, fill="green")
+    
+    def calc_normalized_width(self, number_of_rectangles, margin_right=0):
+        rect_width = (self.width - margin_right) / (number_of_rectangles)
+        return rect_width
+    
+    def calc_normalized_height(self, y_max, y, margin_top=10):
+        max_height = (y/y_max) * (self.height - margin_top)
+
+        return max_height
+
+    def calc_max_height(self, years_data):
+        return years_data[-1].finalTotalFunds
+
+    def draw_rectangle(self, x1, y1, x2, y2, outline="black", fill="blue"):
+        return self.canvas.create_rectangle(x1, y1, x2, y2,
+                                            outline = outline, fill = fill,
+                                            width = 2)
+                                            
